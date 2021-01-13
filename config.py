7d73c4bff876e8363ff69f75696ea84ea5675945
Ilito Torquato <ilito.torquato@joyjet.com>
2021-01-13 15:15:37 -0300

    initial configs

---
+++ config.py
@@ -0,0 +1,16 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@localhost/flask_contacts"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = ""
+    
+class Development(Config):
+    Debug=True
+    
+class Testing(Config):
+    pass
+
+
+config = {
+    "development": Development,
+    "testing": Testing
+}
